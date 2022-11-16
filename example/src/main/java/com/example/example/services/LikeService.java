c507a8ec552dee1568aa9517cac971141ab15eb3
Berk <70212751+BerkTas20@users.noreply.github.com>
2022-11-16 10:32:07 +0300

    update

--- example/src/main/java/com/example/example/services/LikeService.java
+++ example/src/main/java/com/example/example/services/LikeService.java
@@ -1,14 +1,9 @@
 package com.example.example.services;
 
-import com.example.example.entities.Like;
-import com.example.example.entities.Post;
-import com.example.example.entities.User;
-import com.example.example.repository.LikeRepository;
-import com.example.example.requests.LikeCreateRequest;
-import com.example.example.responses.LikeResponse;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Lazy;
+import com.example.example.entities.Like;
+import com.example.example.repository.LikeRepository;
+import com.example.example.responses.LikeResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,7 +13,28 @@ import java.util.stream.Collectors;
 @Service
 public class LikeService {
 
+    private LikeRepository likeRepository;
+    private UserService userService;
+    private PostService postService;
 
+    public LikeService(LikeRepository likeRepository, UserService userService, PostService postService) {
+        this.likeRepository = likeRepository;
+        this.userService = userService;
+        this.postService = postService;
+    }
+
+    public List<LikeResponse> getAllLikesWithParam(Optional<Long> userId, Optional<Long> postId){
+        List<Like>list;
+        if (userId.isPresent() && postId.isPresent()){
+            list = likeRepository.findByUserIdAndPostId(userId.get(), postId.get());
+        } else if (userId.isPresent()) {
+            list = likeRepository.findByPostId(postId.get());
+        } else if (postId.isPresent()) {
+            list = likeRepository.findByPostId(postId.get());
+        }else
+            list = likeRepository.findAll();
+        return list.stream().map(like -> new LikeResponse(like)).collect(Collectors.toList());
+    }
 }
 
 
